d6f7020ccf3aa8bc2e858b03190a47a3d1826de1
fatemeh tabatabai <fatemehtabatabai32@gmail.com>
2025-12-26 14:22:53 -0800

    done2

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
-const inputOne = document.getElementById("label1");
-const inputTwo = document.getElementById("label2");
-const button = document.getElementById("btnone");
-const result = document.getElementById("spanbtn");
-
-button.addEventListener("click", () => {
-  const valueOne = Number(inputOne.value);
-  const valueTwo = Number(inputTwo.value);
-
-  result.textContent = valueOne + valueTwo;
+const inputone = document.getElementById("label1");
+const inputtwo = document.getElementById("label2");
+const buttom1 = document.getElementById("btnone");
+const sum = document.getElementById("spanbtn");
+console.log(inputone);
+console.log(inputtwo);
+console.log(buttom1);
+console.log(sum);
+buttom1.addEventListener("click", () => {
+  const inputOneValue = Number(inputone.value);
+  const inputTwoValue = Number(inputtwo.value);
+  console.log(inputOneValue);
+  console.log(inputTwoValue);
+  sum.textContent = inputOneValue + inputTwoValue;
 });
